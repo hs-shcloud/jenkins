f7fed8279a3c725798e402150d3d202ce15e4394
hs-shcloud <71588519+hs-shcloud@users.noreply.github.com>
2020-09-20 15:22:09 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_cidr}"
+
+  tags {
+    Name = "shcloud_vpc"
+  }
+}
+
+resource "aws_subnet" "main_subnet1" {
+  count             = "${length(var.azs)}"
+  availability_zone = "${element(var.azs,count.index)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(var.subnet_cidr,count.index)}"
+
+  tags {
+    Name     = "Subnet-${count.index+1}"
+    Location = "Banglore"
+  }
+}
